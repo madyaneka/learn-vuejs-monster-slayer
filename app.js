098479c2695d2c133593989726f413897aa936a0
Madyan Eka <madyan.eka95@gmail.com>
2018-10-07 08:05:32 +0700

    create attack method and method templates for special, heal, giveup

--- app.js
+++ app.js
@@ -12,6 +12,35 @@ new Vue({
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
-    }
+    },
+
+    attack() {
+      let dmg = (maxDmg, minDmg) => Math.max(Math.floor(Math.random() * maxDmg) + 1, minDmg)
+
+      let monsterMaxDmg = 10
+      let monsterMinDmg = 3
+      this.monsterHealth -= dmg(monsterMaxDmg, monsterMinDmg)
+
+      if (this.monsterHealth <= 0) {
+        alert('You won!')
+        this.gameIsRunning = false
+        return
+      }
+
+      let playerMaxDmg = 12
+      let playerMinDmg = 5
+      this.playerHealth -= dmg(playerMaxDmg, playerMinDmg)
+
+      if (this.playerHealth <= 0) {
+        alert('You lost!')
+        this.gameIsRunning = false
+      }
+    },
+
+    specialAttack() {},
+
+    heal() {},
+
+    giveUp() {}
   }
 })
